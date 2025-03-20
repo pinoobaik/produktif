fa41e692b5cdb40a16c95f40b56ffd7bcd0a7cea
pinoobaik <venorenand27@gmail.com>
2025-03-20 14:14:36 +0700

    acara 18

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Backend\DashboardController;
 use App\Http\Controllers\ManagementUserController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\CobaController;
 
 // Route dasar
 Route::get('/', function () {
@@ -53,7 +54,7 @@ Route::group(['namespace' => 'App\Http\Controllers\backend'], function () {
     Route::resource('pendidikan', PendidikanController::class);
 });
 
-//acara17
+//acara17 dan 18
 Route::get('/session/create', [SessionController::class, 'create']);
 Route::get('/session/show', [SessionController::class, 'show']);
 Route::get('/session/delete', [SessionController::class, 'delete']);
@@ -62,3 +63,5 @@ Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
 
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/cobaerror/{nama?}', [CobaController::class, 'index']);
